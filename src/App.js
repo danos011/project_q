5dc2ae8509f9cc375377bfe355ada406868f56ec
danos011 <108811135+danos011@users.noreply.github.com>
2024-08-12 19:48:31 +0300

    rework vid

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {Portfolio} from "./Components/Portfolio";
 function App() {
     const [intro, setIntro] = useState(true);
     const [fadeOut, setFadeOut] = useState(false);
+    const [classname, setClassName] = useState('');
 
     useEffect(() => {
         if (!intro) {
@@ -22,6 +23,7 @@ function App() {
     }, [intro]);
 
     useEffect(() => {
+        setClassName(`z-[-1] w-full object-cover h-[100vh] ${fadeOut ? 'fade-out' : ''}`)
         setTimeout(() => {
             setIntro(false)
         }, 3620);
@@ -41,25 +43,29 @@ function App() {
                     }`}</style>
             {
                 intro ?
-                    <video autoPlay muted src="/video/intro.mp4"
-                           className={`z-[-1] w-full object-cover h-[100vh] ${fadeOut ? 'fade-out' : ''}`}></video>
+
+                    <video controls={false} autoPlay={true} playsInline={true} muted={true} src="/video/intro_transcoded.mp4"
+                           className={classname}></video>
                     :
                     <NextUIProvider className={"container max-w-full overflow-y-hidden"}>
                         <Nav/>
                         <div className={`flex flex-col gap-[10vh]`}>
                             <Project fadeOut={fadeOut}/>
 
-                            <AboutUs title={"Сферы"} belowTitle={"деятельности"} boxTitle={"Кто"} coloredBoxTitle={"мы ?"}
+                            <AboutUs title={"Сферы"} belowTitle={"деятельности"} boxTitle={"Кто"}
+                                     coloredBoxTitle={"мы ?"}
                                      boxDescription={"Q Venture Capital компания, специализирующаяся на разработке " +
                                          "программного обеспечения в сфере финансовых технологий, торговлей (трейдинг) " +
                                          "и венчурным инвестированием в инновационные проекты в WEB3."}/>
 
-                            <PageWithText title={'web'} coloredText={'3'} afterTitle={"Инновационные решения"} textId={'web3'}
+                            <PageWithText title={'web'} coloredText={'3'} afterTitle={"Инновационные решения"}
+                                          textId={'web3'}
                                           description={"Разработка децентрализованных финансовых платформ, смарт-контрактов, " +
                                               "NFT, кошельков для криптовалют и других решений, использующих технологии " +
                                               "блокчейн для улучшения качества финансовых услуг и создания новых бизнес-моделей."}/>
 
-                            <PageWithText title={'трей'} coloredText={'динг'} afterTitle={"торговые операции"} textId={'trading'}
+                            <PageWithText title={'трей'} coloredText={'динг'} afterTitle={"торговые операции"}
+                                          textId={'trading'}
                                           description={"Компьютерный алгоритм, который автоматически анализирует ситуацию " +
                                               "на финансовом рынке и предоставляет подробную информацию, используя торговые " +
                                               "индикаторы, а также искусственный интеллект для анализа рынка.\n" +
